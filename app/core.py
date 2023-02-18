b6346585adf732f453df20f767111b9f84db942d
arkadiysotnikov <cotnikoarkady@gmail.com>
2023-02-18 15:56:10 +0400

    Create unit test for base64 utils

--- app/core.py
+++ app/core.py
@@ -4,9 +4,10 @@ import config
 from app.image_utils import decode_base64_to_image
 
 logging.basicConfig(level=config.LOGLEVEL)
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 def convert_image_to_text(payload: dict) -> dict:
+    log.info(f"Image size: payload['image_size']")
     image = decode_base64_to_image(payload["image"])
     return "Hello API!"
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,19 +1,17 @@
-from os.path import normpath, dirname
-from pathlib import Path
 from unittest import TestCase
 import os
 from app.core import convert_image_to_text
 import cv2
-
+from tests.utils import test_dir_path
 from app.image_utils import encode_image_to_base64
 
 
-class Test(TestCase):
+class CoreTest(TestCase):
     def test_convert_image_to_text(self):
-        test_dir_path = Path(normpath(dirname(__file__)))
         test_image = cv2.imread(os.path.join(test_dir_path, "data", "1.png"))
         test_image_shape = test_image.shape
         test_image = encode_image_to_base64(test_image)
         test_image_payload = {"image": test_image, "img_size": test_image_shape}
-        self.assertIsInstance(convert_image_to_text(test_image_payload), str)
-        self.assertIsNotNone(convert_image_to_text(test_image_payload))
+        recognized_text = convert_image_to_text(test_image_payload)
+        self.assertIsInstance(recognized_text, str)
+        self.assertIsNotNone(recognized_text)
